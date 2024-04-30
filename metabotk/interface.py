c9a6c4287bd0f5c2964673ecea59a1ed9ee56b3a
matteobolner <matteo.bolner2@unibo.it>
2024-04-30 11:01:38 +0200

    Moved statistics functions to statistics module

--- metabotk/interface.py
+++ metabotk/interface.py
@@ -28,89 +28,12 @@ class MetaboTK(DatasetManager):
         )
         self.stats = StatisticsHandler()
 
-    ###
-    # Statistics
-    ###
-
-    def sample_stats(self, outlier_threshold=5, exclude_xenobiotics=True):
-        """
-        Computes basic statistics for the metabolomics data sample-wise
-
-        Xenobiotic metabolites are usually not considered in the calculations
-        This function computes statistics for each sample across all metabolites.
-
-        Parameters:
-            outlier_threshold (int): Threshold for identifying outliers.
-            exclude_xenobiotics (bool): If True, xenobiotic metabolites are excluded from the calculation.
-
-        Returns:
-            pandas DataFrame: DataFrame containing statistics for each sample across all metabolites.
-        """
-        # Ensure that data is set up properly
-        if self.data.empty:
-            raise ValueError(
-                "No data available. Please import data before computing statistics."
-            )
-
-        # Compute statistics using StatisticsHandler
-        if exclude_xenobiotics:
-            data = self.drop_xenobiotic_metabolites(inplace=False)
-        else:
-            data = self.data
-        sample_stats = self.stats.compute_dataframe_statistics(
-            data, outlier_threshold, axis=1
-        )
-
-        # Compute Total Sum of Abundance (TSA) for each sample across all metabolites
-        tsa_complete_only = self.stats.total_sum_abundance(
-            data, exclude_incomplete=True
-        )
-        tsa_complete_only.name = "TSA_complete_only"
-        tsa_all = self.stats.total_sum_abundance(data, exclude_incomplete=False)
-        tsa_all.name = "TSA_including_incomplete"
-        tsa = pd.concat([tsa_complete_only, tsa_all], axis=1)
-
-        # Merge TSA with sample statistics
-        sample_stats = sample_stats.merge(tsa, left_index=True, right_index=True)
-        # self.sample_stats=sample_stats
-        return sample_stats
-
-    def metabolite_stats(self, outlier_threshold=5):
-        """
-        Computes basic statistics for the metabolomics data metabolite-wise
-
-        This function computes basic statistics for each metabolite in the data.
-        The statistics computed are:
-        - Mean
-        - Standard deviation
-        - Median
-        - Min
-        - Max
-        - Sum
-        - Coefficient of Variation (CV%)
-        - Number of missing values
-        - Number of outliers
-
-        Parameters:
-        - outlier_threshold: Threshold for outlier detection (default=5).
-
-        Returns:
-            pandas DataFrame: DataFrame containing statistics for each metabolite.
-            The index of the DataFrame is the metabolite names.
-
-        """
-        # Ensure that data is set up properly
-        if self.data.empty:
-            raise ValueError(
-                "No data available. Please import data before computing statistics."
-            )
-
-        # Compute statistics using StatisticsHandler
-        metabolite_stats = self.stats.compute_dataframe_statistics(
-            self.data, outlier_threshold, axis=0
-        )
-        # self.metabolite_stats=metabolite_stats
-        return metabolite_stats
+    @property
+    def stats(self):
+        """Lazy initialization of StatisticsHandler instance."""
+        if not hasattr(self, "_stats_"):
+            self._models_ = StatisticsHandler()
+        return self._models_
 
     @property
     def models(self):
